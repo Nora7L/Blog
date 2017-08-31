3a74324d5fe503eebf9fc0da407358dfb7c69f30
Nora7L <Naro7L@163.com>
2017-08-31 21:18:16 +0800

    两个功能
    回到顶部和固定导航

---
+++ js/index.js
@@ -0,0 +1,33 @@
+/*点击回到顶部*/
+$(function(){
+	$(window).scroll(function(){
+		var scrollTop=$(document).scrollTop();
+		if (scrollTop>500) {
+			$('.top').fadeIn('slow')
+		}else{
+			$('.top').fadeOut("500")
+		}
+    });
+    $('.top').click(function(){
+    	$("html,body").animate({scrollTop:0}, 500);
+    })
+})
+/*顶部跟随效果*/
+$(function(){
+   $(window).scroll(function(){
+		var scrollTop=$(document).scrollTop();
+		var headerH=$('.header').height();
+		// console.log(scrollTop)
+		if (scrollTop>headerH+60) {
+			$('.header').css({
+				'position':'fixed',
+				'top':"0"
+		    });
+		    $('.section').css('marginTop','120px')
+		}else{
+            $('.header').css('position','static');
+		    $('.section').css('marginTop','60px')
+		}
+   })
+});
+/*分页*/
