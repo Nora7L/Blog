b6dfb189b0853f045d85ddac5b20baafee836173
Nora7L <Naro7L@163.com>
2017-09-06 20:20:21 +0800

    9.6算法

--- js/data.js
+++ js/data.js
@@ -18,7 +18,7 @@ data={
 		"aclass":"dome"
 	},
 	{
-		"title":"个人简历--给自己一个666",
+		"title":"前端常见的算法",
 		"data":"2017.8.29",
 		"class":"随笔",
 		"img":"img/bg2.jpg",
@@ -53,6 +53,28 @@ data={
 	"content":[
 	{
        "data":"    这是一篇长文，按照我自己的逻辑重新整理一下,包含所有Vue的基础知识点。但是我更建议你先简略的阅读官方的文档，因为本文具有一定的阅读门槛，同时我也竭尽所能把这门槛降到最低，同样你也可以把本文作为快速回忆教程。"
+	},
+	{
+       "data":'<h1 style="text-align:center">排序算法</h1><br><h2>1、冒泡排序</h2><br><img src="../img/冒泡排序.png">'
+       +'<br><h2>2、快速排序</h2><br><img src="../img/2快速排序.png">'
+       +'<br><h2>3、二路归并</h2><br><img src="../img/3二路并归.png">'
+       +'<br><br><h1 style="text-align:center">排序算法</h1>'
+       +'<br><h2>1、判断回文字符串</h2><br><img src="../img/3二路并归.png">'
+       +'<br><h2>2、翻转字符串</h2><br><h3>思路一：反向遍历字符串</h3><br><img src="../img/思路一：反向遍历字符串.png">'
+       +'<br><h3>思路二：转化成array操作</h3><br><img src="../img/转化成array操作.png">'
+       +'<br><h2>3、生成指定长度随机字符串</h2><br><img src="../img/生成指定长度随机字符串.png">'
+       +'<br><h2>4、统计字符串中次数最多字母</h2><br><img src="../img/统计字符串中次数最多字母.png">'
+       +'<br><br><h1 style="text-align:center">数组操作</h1>'
+       +'<br><h2>1、数组去重</h2><br><img src="../img/数组去重.png">'
+       +'<br><h2>2、数组中最大差值</h2><br><img src="../img/数组中最大差值.png">'
+       +'<br><br><h1 style="text-align:center">其他常见算法</h1>'
+       +'<br><h2>1、阶乘</h2><br><h3>非递归实现</h3><br><img src="../img/非递归实现.png">'
+       +'<br><h3>递归实现</h3><br><img src="../img/递归实现.png">'
+       +'<br><h2>2、生成菲波那切数列</h2><br><h3>强行递归实现</h3><br><img src="../img/强行递归实现.png">'
+       +'<br><h2>简约非递归实现</h2><br><img src="../img/简约非递归实现.png">'
+       +'<br><h2>3、二分查找</h2><br><h3>非递归实现</h3><br><img src="../img/非递归实现 (二分).png">'
+       +'<br><h2>递归实现</h2><br><img src="../img/递归实现（二分）.png">'
+
 	}
 	]
 
