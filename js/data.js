2f22e413e6e1d29d1ab5f114502e82ac8770e5d0
Nora7L <Naro7L@163.com>
2017-09-07 10:15:27 +0800

    代码块的引入

--- js/data.js
+++ js/data.js
@@ -52,7 +52,9 @@ data={
 	],
 	"content":[
 	{
-       "data":"    这是一篇长文，按照我自己的逻辑重新整理一下,包含所有Vue的基础知识点。但是我更建议你先简略的阅读官方的文档，因为本文具有一定的阅读门槛，同时我也竭尽所能把这门槛降到最低，同样你也可以把本文作为快速回忆教程。"
+       "data":`<h5>    自己搭建的博客怎么看都是好的</h5><br>`
+               +`<h5>2017.8.24</h5><br><p>    搭建好了了自己的第一个页面，用时两周。长长的lis里终于搞定了一项，超级鸡冻啊！发布的时候，手都是抖的！这么久以来，自己慢慢学，一点点看，终于有了一点收获，这也是对自己的一点肯定
+现在的我非常有动力，继续努力下去了！<br>fighting！</p>`
 	},
 	{
        "data":'<h1 style="text-align:center">排序算法</h1><br><h2>1、冒泡排序</h2><br><img src="http://ovuz0ozqd.bkt.clouddn.com/%E5%86%92%E6%B3%A1%E6%8E%92%E5%BA%8F.png">'
@@ -75,7 +77,466 @@ data={
        +'<br><h2>3、二分查找</h2><br><h3>非递归实现</h3><br><img src="http://ovuz0ozqd.bkt.clouddn.com/%E9%9D%9E%E9%80%92%E5%BD%92%E5%AE%9E%E7%8E%B0%20%28%E4%BA%8C%E5%88%86%29.png">'
        +'<br><h2>递归实现</h2><br><img src="http://ovuz0ozqd.bkt.clouddn.com/%E9%80%92%E5%BD%92%E5%AE%9E%E7%8E%B0%EF%BC%88%E4%BA%8C%E5%88%86%EF%BC%89.png">'
 
-	}
+	},
+	{
+       "data":`<p>    用了几天的时间把之前写的小游戏俄罗斯方块改了一下，下面贴代码：</p>`
+       +`<div class="heightDm" style="margin:-45px 0px;">
+       <pre class="brush: js">
+       <!DOCTYPE html>
+       <html lang="en">
+       <head>
+       <meta charset="UTF-8">
+       <title>俄罗斯方块--完美注释</title>
+       <style type="text/css">
+       .bgImg{
+       	width: 500px;
+       	height: 620px;
+       	background-image: url(650.png);
+       	background-color: rgba(86,219,218,0.4);
+       	position: relative;
+       	left: 0;
+       	right: 0;
+       	margin: 100px auto;
+       }
+       #score{
+       	display: block;
+       	position: absolute;
+       	right: 45px;
+       	top: 50%;
+       	font-size: 30px;
+
+       }
+       #alls{
+       	display: block;
+       	position: absolute;
+       	right: 35px;
+       	top: 43%;
+       	font-size: 30px;
+       }
+       div>img{
+       	position: absolute;
+       	right: 20px;
+       	top: 27%;
+       }
+       tbody{
+       	position: absolute;
+       	left:115px;
+       	top: 80px;
+
+       }
+       #board tr td{ 
+       	background-color: #F4A460;
+       	width: 25px; 
+       	height: 25px; 
+       }
+       .over{
+       	font-size: 50px;
+       	position: absolute;
+       	z-index: 9999;
+       	left: 145px;
+       	top: 30%;
+       	display: none;
+       }
+       .block{
+
+       }
+
+       </style>
+       </head>
+       <body>
+       <div class="bgImg" id="bgImg">
+       <img  value="开始" src="center.png"  onclick="begin(this)">
+       <span id="alls">得分：</span>
+       <span id="score"> 0</span>
+       <p class="over" id="over">游戏结束</p>
+       </div>
+       <script language="javascript">
+       /*动态生成table*/
+       window.onload=function(){
+       	var div=document.getElementById('bgImg');
+       	var str="",
+       	a="";
+       	for (var k = 0; k < 10; k++) {
+       		str+='<td></td>'
+       	}
+       	for (var i = 0; i <18; i++) {
+       		a+='<tr>'+str+'</tr>';   
+       	}
+       	div.innerHTML+=
+       	'<table id="board" cellspacing=0 cellpadding=0 border=1 
+       	style="border-collapse: collapse;">'
+       	+a+'</table>';
+       }
+       var tbl;//游戏主面板
+       var status=0;////游戏状态 0: 未开始;1 运行; 2 中止; 
+       var timer;//定时器
+       var score = 0; //分数
+       //board是一个18*10的数组
+       var board= new Array(18);
+       for (var i = 0; i <18; i++) {
+       	board[i]=new Array(10);
+       }
+       // console.log(board);
+       //用来标注那些方格已经被占据. 初始时都为0, 如果被占据则为1
+       for (var i = 0; i <18; i++) {
+       	for (var j = 0; j < 10; j++) {
+       		board[i][j]=0;
+       	}
+       }
+       // console.log(board);
+
+       //定义一个当前活动的方块
+       var activeBlock;
+       //定义方块的基本形状--7个
+       function genBlock(){
+       	activeBlock = new Array(4);
+       	//产生随机整数0-6
+       	var t= (Math.floor(Math.random()*20)+1)%7;
+       	switch(t){ 
+       		case 0:{ 
+       			activeBlock[0] = {x:0, y:4}; 
+       			activeBlock[1] = {x:1, y:4}; 
+       			activeBlock[2] = {x:0, y:5}; 
+       			activeBlock[3] = {x:1, y:5}; 
+       			break; 
+       		} 
+       		case 1:{ 
+       			activeBlock[0] = {x:0, y:3}; 
+       			activeBlock[1] = {x:0, y:4}; 
+       			activeBlock[2] = {x:0, y:5}; 
+       			activeBlock[3] = {x:0, y:6}; 
+       			break; 
+       		} 
+       		case 2:{ 
+       			activeBlock[0] = {x:0, y:5}; 
+       			activeBlock[1] = {x:1, y:4}; 
+       			activeBlock[2] = {x:1, y:5}; 
+       			activeBlock[3] = {x:2, y:4}; 
+       			break; 
+       		} 
+       		case 3:{ 
+       			activeBlock[0] = {x:0, y:4}; 
+       			activeBlock[1] = {x:1, y:4}; 
+       			activeBlock[2] = {x:1, y:5}; 
+       			activeBlock[3] = {x:2, y:5}; 
+       			break; 
+       		} 
+       		case 4:{ 
+       			activeBlock[0] = {x:0, y:4}; 
+       			activeBlock[1] = {x:1, y:4}; 
+       			activeBlock[2] = {x:1, y:5}; 
+       			activeBlock[3] = {x:1, y:6}; 
+       			break; 
+       		} 
+       		case 5:{ 
+       			activeBlock[0] = {x:0, y:4}; 
+       			activeBlock[1] = {x:1, y:4}; 
+       			activeBlock[2] = {x:2, y:4}; 
+       			activeBlock[3] = {x:2, y:5}; 
+       			break; 
+       		} 
+       		case 6:{ 
+       			activeBlock[0] = {x:0, y:5}; 
+       			activeBlock[1] = {x:1, y:4}; 
+       			activeBlock[2] = {x:1, y:5}; 
+       			activeBlock[3] = {x:1, y:6}; 
+       			break; 
+       		} 
+       	} 
+       	//检查小方块的位置是否可以放在初始位置
+       	for (var i = 0; i <4; i++) {
+       		if (!isCellValid(activeBlock[i].x,activeBlock[i].y)) {
+       			return false;
+       		}else{
+       			return true;
+       		}
+       	}
+       }
+       //向下移动
+       function moveDown(){
+       	//首先检查底边界 看有没有触底
+       	if (checkBottomBorder()) {
+       		//没有
+       		erase();
+       		for (var i = 0; i <4; i++) {
+       			activeBlock[i].x=activeBlock[i].x+1;
+       		}
+       		paint();
+       	}else{
+       		//触底
+       		//停止自动向下移动
+       		clearInterval(timer);
+       		//更新数组
+       		updateBoard();
+       		//消行
+       		var lines=deleteLine();
+       		//有消行就刷新分数
+       		if (lines!=0) {
+       			score=score+lines*10;
+       			updateScore();
+       			//擦除面板
+       			eraseBoard();
+       			//重绘面板
+       			paintBoard();
+       		}
+       		//产生新图行。并判断是否可以放在最初位置
+
+       		var oP=document.getElementById('over');
+       		if (!genBlock()) {
+       			oP.style.display='block';
+       			status=2;
+       			return;
+       		}
+       		paint();
+       		//定时器
+       		timer=setInterval(moveDown,1000);
+       	}
+       }
+       //左移动
+       function moveLeft(){
+       	if (checkLeftBorder()) {
+       		erase();
+       		for (var i = 0; i <4; i++) {
+       			activeBlock[i].y=activeBlock[i].y-1;
+       		}
+       		paint();
+       	}
+       }
+       //右移动
+       function moveRight(){
+       	if (checkRightBorder()) {
+       		erase();
+       		for (var i = 0; i <4; i++) {
+       			activeBlock[i].y=activeBlock[i].y+1;
+       		}
+       		paint();
+       	}
+       }
+       //旋转
+       //因为旋转之后可能会有方格覆盖已有的方格. 
+       //先用一个tmpBlock,把activeBlock的内容都拷贝到tmpBlock, 
+       //对tmpBlock尝试旋转,如果旋转后检测发现没有方格产生冲突
+       //再把旋转后的tmpBlock的值给activeBlock. 
+       function rotate(){
+       	var tmpBlock=new Array(4);
+       	for (var i = 0; i <4; i++) {
+       		tmpBlock[i]={x:0,y:0};
+       	}
+       	for (var i = 0; i <4; i++) {
+       		tmpBlock[i].x=activeBlock[i].x;
+       		tmpBlock[i].y=activeBlock[i].y;
+       	}
+       	//先算四个点的中心点，则这四个点围绕中心旋转90度。 
+       	var cx = Math.round((tmpBlock[0].x + tmpBlock[1].x + 
+             	tmpBlock[2].x + tmpBlock[3].x)/4); 
+       	var cy = Math.round((tmpBlock[0].y + tmpBlock[1].y + 
+             	tmpBlock[2].y + tmpBlock[3].y)/4); 
+       	//先假设围绕源点旋转。然后再加上中心点的坐标。 
+       	for (var i = 0; i <4; i++) {
+       		tmpBlock[i].x=cx+cy-activeBlock[i].y; 
+       		tmpBlock[i].y=cy-cx+activeBlock[i].x; 
+       	}
+       	//检查旋转后方格是否不重叠
+       	for (var i = 0; i < 4; i++) {
+       		if (!isCellValid(tmpBlock[i].x,tmpBlock[i].y)) {
+       			return;
+       		}
+       	}		
+       	erase();
+
+
+       	for (var i = 0; i < 4; i++) {
+       		activeBlock[i].x=tmpBlock[i].x;
+       		activeBlock[i].y=tmpBlock[i].y;
+       	}
+       	paint();
+       }
+       //检查方块(x，y)是否已经存在在border中
+       function isCellValid(x,y){
+       	if (x>17||x<0||y>9||y<0) {
+       		return false;
+       	}else if (board[x][y]==1) {
+       		return false;
+       	}else{
+       		return true;
+       	}
+       }
+       //检查底边界
+       function checkBottomBorder(){ 
+       	for(var i=0; i<activeBlock.length; i++){ 
+       		if(activeBlock[i].x==17){ 
+       			return false; 
+       		} 
+       		if(!isCellValid(activeBlock[i].x+1, activeBlock[i].y)){ 
+       			return false; 
+       		} 
+       	} 
+       	return true; 
+       }
+       //检查左边界
+       function checkLeftBorder(){
+       	for (var i = 0; i < activeBlock.length; i++) {
+       		if (activeBlock[i].y==0) {
+       			return false;
+       		}
+       		if (!isCellValid(activeBlock[i].x,activeBlock[i].y-1)) {
+       			return false;
+       		}
+
+       	}
+       	return true;
+       }
+       //检查右边界
+       function checkRightBorder(){ 
+       	for(var i=0; i<activeBlock.length; i++){ 
+       		if(activeBlock[i].y==9){ 
+       			return false; 
+       		} 
+       		if(!isCellValid(activeBlock[i].x, activeBlock[i].y+1)){ 
+       			return false; 
+       		} 
+       	} 
+       	return true; 
+       } 
+       //擦除
+       function erase(){
+       	for (var i = 0; i <4; i++) {
+       		tbl.rows[activeBlock[i].x]
+       		.cells[activeBlock[i].y]
+       		.style.backgroundColor="#F4A460";
+       	}
+       }
+       //擦除整个面板
+       function eraseBoard(){
+       	for (var i = 0; i <18; i++) {
+       		for (var j = 0; j < 10; j++) {
+       			tbl.rows[i].cells[j].style.backgroundColor = "#F4A460"; 
+       		}
+       	}
+       }
+
+       //产生一个空白行
+       function generateBlankLine(){
+       	var line=new Array(10);
+       	for (var i = 0; i <10; i++) {
+       		line[i]=0;
+       	}
+       	return line;
+       }
+       //消除行,改变数组border
+       function updateBoard(){ 
+       	for(var i=0; i<4; i++){ 
+       		board[activeBlock[i].x][activeBlock[i].y]=1; 
+       	} 
+       } 
+       //消除行
+       function deleteLine (){
+       	var lines=0;
+       	for (var i = 0; i <18; i++) {
+       		var j = 0
+       		for (; j <10; j++) {
+       			if (board[i][j]==0) {
+       				break;
+       			}
+       		}
+       		if (j==10) {
+       			lines++
+       			if(i!=0) {
+       				for (var k =i-1; k>=0; k--) {
+       					board[k+1]=board[k];
+       				}
+       			}
+       			board[0]=generateBlankLine();
+
+       		}
+       	}
+       	return lines;
+       }
+       //更新分数
+       function updateScore(){
+       	document.getElementById('score').innerText=""+score;
+       }
+       //重绘整个面板
+       var arrColor=['#1E90FF','#9A32CD','#6495ED','#7B68EE'];
+       var num=0;
+       function paintBoard(){
+       	for (var i = 0; i <18; i++) {
+       		for (var j = 0; j <10; j++) {
+       			num++;
+       			num%=arrColor.length;
+       			if (board[i][j]==1) {
+       				tbl.rows[i]
+       				.cells[j]
+       				.style
+       				.backgroundColor =arrColor[num];
+
+       			}
+       		}
+       	}
+       }
+
+       //键盘事件
+       function keyControl(){
+       	if (status!=1) {
+       		return;
+       	}
+       	var code=event.keyCode;
+       	switch(code){
+       		case 37:{
+       			moveLeft();
+       			break;
+       		}
+       		case 38:{ 
+       			rotate(); 
+       			break; 
+       		} 
+       		case 39:{ 
+       			moveRight(); 
+       			break; 
+       		} 
+       		case 40:{ 
+       			moveDown(); 
+       			break; 
+       		} 
+       	}
+       }
+       //活动方块
+       function paint(){
+       	for(var i=0; i<4; i++){ 
+       		num++;
+       		num%=arrColor.length;
+       		tbl.rows[activeBlock[i].x]
+       		.cells[activeBlock[i].y]
+       		.style.backgroundColor=arrColor[num]; 
+
+
+       	} 
+       } 
+       //开始
+       function begin(e){
+       	e.disabled=true;
+       	status=1;
+       	tbl=document.getElementById('board');
+
+       	var oP=document.getElementById('over');
+       	if(!genBlock()){ 
+       		oP.style.display='block';
+       		status = 2; 
+       		return; 
+       	} 
+       	paint();
+       	timer=setInterval(moveDown,1000);
+       }
+       document.onkeydown=keyControl;
+       </script>
+       </body>
+       </html>
+       </pre>
+       </div>`
+       +`<p>还有两张图片，我就不贴了，这个可以直接替换的<br>`
+       +`<br><h5><a href="https://naro7l.coding.me/game-el/" style="color:black;">这是dome地址</a></h5><br>`
+       +`<h5><a href="https://github.com/Nora7L/js--game" style="color:black;">这是源码地址</a></h5><br>`
+	},
 	]
 
 
